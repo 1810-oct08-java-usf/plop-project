bd9a7ac908978c58505ef35b33c12927f3cb0a82
sadiki <sxs6590@rit.edu>
2018-12-04 13:29:48 -0500

    Embedded MongoDB integrated.

--- src/main/java/com/revature/controllers/ProjectController.java
+++ src/main/java/com/revature/controllers/ProjectController.java
@@ -14,7 +14,6 @@ import com.revature.models.Project;
 import com.revature.repositorys.ProjectRepository;
 
 @RestController
-@RequestMapping("/")
 public class ProjectController {
 
 	private Environment env;
@@ -25,28 +24,6 @@ public class ProjectController {
 		this.env = env;
 		this.projectRepo = projectRepo;
 	}
-
-//	@GetMapping("/projects")
-//	@ResponseStatus(HttpStatus.OK)
-//	public List<Project> getProjects(){
-//		
-//		List<AppUser> trainer = new ArrayList<AppUser>();
-//		trainer.add(new AppUser(1, "omar", "Unimportant", "USER"));
-//		trainer.add(new AppUser(2, "admin", "Unimportant", "ADMIN"));
-//		List<String> groupMembers = new ArrayList<String>();
-//		groupMembers.add("Sadiki");
-//		groupMembers.add("Paul");
-//		groupMembers.add("Miles");
-//		groupMembers.add("Caleb");
-//		groupMembers.add("Derek");
-//		List<String> screenShots = new ArrayList<String>();
-//		screenShots.add("!");
-//		List<String> zipLinks = new ArrayList<String>();
-//		zipLinks.add("....///");
-//		zipLinks.add("//.....");
-//		return Arrays.asList(new Project("RPM","Bunker Batch",
-//				trainer,groupMembers,screenShots, zipLinks, "DESCRPTION","Java/J2EE", 1));
-//	}
 
 	@GetMapping("/projects")
 	@ResponseStatus(HttpStatus.OK)
@@ -60,15 +37,5 @@ public class ProjectController {
 		System.out.println(projectRepo.findAll());
 		return projectRepo.findAll();
 	}
-
-//	@GetMapping("/projects")
-//	@ResponseStatus(HttpStatus.OK)
-//	public Project getThisProject() {
-//		return projectRepo.
-//	}
-//	
-//	@RequestMapping(value="/{id}", method= RequestMethod.GET)
-//	public Project getProjectById(@PathVariable("id") ObjectId id) {
-//		return projectRepo.findById(id);
-//	}
+	
 }
