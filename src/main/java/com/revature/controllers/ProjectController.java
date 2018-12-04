37ce65e2a7b1ce9a11590afc72696648d1ae0952
sadiki <sxs6590@rit.edu>
2018-12-04 16:31:51 -0500

    Able to find project by name, batch, stack, username, and status. Can delete and update.

--- src/main/java/com/revature/controllers/ProjectController.java
+++ src/main/java/com/revature/controllers/ProjectController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,13 +30,75 @@ public class ProjectController {
 		this.projectRepo = projectRepo;
 	}
 
-	@GetMapping("/projects")
+	// Add new project
+	@PostMapping("/add")
 	@ResponseStatus(HttpStatus.OK)
-	public List<Project> getProjects(){
-		// Get all projects
-				System.out.println("Projects found with findAll()");
-				System.out.println("-------------------------------");
-				System.out.println(projectRepo.findAll());
-				return projectRepo.findAll();
+	public Boolean addProject(@RequestBody Project project) {
+		if(project != null) {
+			projectRepo.save(project);
+			return true;
+		}else
+		{
+			return false;
+		}
+		
+	}
+	
+	// Get all projects
+	@GetMapping("/")
+	@ResponseStatus(HttpStatus.OK)
+	public List<Project> getSpecifiedProjects(
+			@RequestParam(value ="name", required = false) String name,
+			@RequestParam(value ="batch", required = false) String batch,
+			@RequestParam(value ="fullName", required = false) String fullName,
+			@RequestParam(value ="techStack", required = false) String techStack,
+			@RequestParam(value ="status", required = false) String status){
+		
+		if(name != null) {
+			System.out.println(name);
+			return projectRepo.findByName(name);
+		} 
+		else if(batch != null) {
+			return projectRepo.findByBatch(batch);
+		} 
+		else if (fullName != null) {
+			return projectRepo.findByFullName(fullName);
+		}
+		else if (techStack != null) {
+			return projectRepo.findByTechStack(techStack);
+		}
+		else if(status != null) {
+			return projectRepo.findByStatus(status);
+		}
+		else {
+			return projectRepo.findAll();
+		}
+	
+	}
+	
+	
+	// Delete by ID
+	@DeleteMapping("/delete/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Boolean deleteById(@PathVariable String id) {
+		if(id != null) {
+			projectRepo.deleteById(id);
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	// Update Project
+	@PostMapping("/update")
+	@ResponseStatus(HttpStatus.OK)
+	public Boolean updateByName(@RequestBody Project project) {
+		if(project != null) {
+			projectRepo.save(project);
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
 }
--- src/main/java/com/revature/repositories/ProjectRepository.java
+++ src/main/java/com/revature/repositories/ProjectRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.revature.models.Project;
  public interface ProjectRepository extends MongoRepository<Project, String>{
- 	public Project findByName(String name);
+ 	public List<Project> findByName(String name);
 	public List<Project> findByBatch(String batch);
+	public List<Project> findByFullName(String name);
+	public List<Project> findByTechStack(String techStack);
+	public List<Project> findByStatus(String status);
 }
