77538d2ed1b591d6bbe914be309261cc3415792a
sadiki <sxs6590@rit.edu>
2018-12-04 13:49:13 -0500

    Important Files didn't successfully push

--- src/main/java/com/revature/ProjectServiceApplication.java
+++ src/main/java/com/revature/ProjectServiceApplication.java
@@ -1,11 +1,16 @@
 package com.revature;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import com.revature.models.Project;
-import com.revature.repositorys.ProjectRepository;
+import com.revature.repositories.ProjectRepository;
 
 @EnableEurekaClient
 @SpringBootApplication
