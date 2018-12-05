f0a51b46cdca1ca05cedfc70255435cbc80c6a3a
sadiki <sxs6590@rit.edu>
2018-12-05 10:08:25 -0500

    Added findbyid. Removed config-service dependency from POM

--- src/main/java/com/revature/service/ProjectService.java
+++ src/main/java/com/revature/service/ProjectService.java
@@ -1,6 +1,7 @@
 package com.revature.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -11,63 +12,74 @@ import com.revature.repositories.ProjectRepository;
 
 @Service
 public class ProjectService {
-	
+
 	ProjectRepository projectRepo;
-	
+
 	public ProjectService(ProjectRepository projectRepo) {
 		this.projectRepo = projectRepo;
 	}
-	
- 	public List<Project> findByName(String name){
+
+	public List<Project> findByName(String name) {
 		return projectRepo.findByName(name);
- 	}
- 	
-	public List<Project> findByBatch(String batch){
+	}
+
+	public List<Project> findByBatch(String batch) {
 		return projectRepo.findByBatch(batch);
 	}
 
-	public List<Project> findByFullName(String fullName){
+	public List<Project> findByFullName(String fullName) {
 		return projectRepo.findByFullName(fullName);
 	}
 
-	public List<Project> findByTechStack(String techStack){
+	public List<Project> findByTechStack(String techStack) {
 		return projectRepo.findByTechStack(techStack);
 	}
 
-	public List<Project> findByStatus(String status){
+	public List<Project> findByStatus(String status) {
 		return projectRepo.findByStatus(status);
 	}
 
-	public List<Project> findAllProjects(){
+	public List<Project> findAllProjects() {
 		return projectRepo.findAll();
 	}
-	
+
 	public Boolean deleteById(String id) {
-		if(id != null) {
+		if (id != null) {
 			projectRepo.deleteById(id);
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	public Boolean updateProject(Project project) {
-		if(project != null) {
+		if (project != null) {
 			projectRepo.save(project);
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	public Boolean addProject(Project project) {
-		if(project != null) {
+		if (project != null) {
 			projectRepo.save(project);
 			return true;
-		}else
-		{
+		} else {
 			return false;
 		}
+
+	}
+
+	public Project findById(String id) {
 		
+		Optional<Project> currProject = projectRepo.findById(id);
+		
+		if(currProject.isPresent()) {
+			return currProject.get();
+		}else
+		{
+			return null;
+		}
 	}
 }
