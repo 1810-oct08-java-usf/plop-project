fd20fb00a593c84bb687a622d8e343c9d212c9a0
Caleb Massey <cabobmassey@gmail.com>
2018-12-18 14:51:31 -0500

    Removed CORS filter

--- src/main/java/com/revature/ProjectServiceApplication.java
+++ src/main/java/com/revature/ProjectServiceApplication.java
@@ -8,6 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.revature.models.Project;
 import com.revature.repositories.ProjectRepository;
