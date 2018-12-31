917984bba2367a1283a6d485e0c50d9fd2426a91
jwjibilian <jwjibilian>
2018-12-31 14:09:27 -0500

    Adding some documentation

--- src/main/java/com/revature/aop/ProjectAspects.java
+++ src/main/java/com/revature/aop/ProjectAspects.java
@@ -12,20 +12,29 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Configuration;
 
-
+/**
+ * Currently used to get extra debug information without having
+ * to scatter code throughout service.
+ * @author Joshua
+ *
+ */
 
 @Aspect
 @Configuration
 public class ProjectAspects {
 	
 	//Logger logger =  LoggerFactory.getLogger(this.getClass());
-	
+//  Used to check if AOP was working
 //	@Before("execution(public * com.revature..*(..))")
 //	public void beforeAnything(JoinPoint joinPoint) {
 //		System.out.println("Before Anything" + joinPoint.getKind());
 //		System.out.println(joinPoint.getStaticPart() + "\n");
 //	}
-	
+	/**
+	 * Used for getting any errors thrown from methods
+	 * @param joinPoint
+	 * @param ex
+	 */
 	@AfterThrowing(pointcut = "execution(* com.revature..*(..))", throwing = "ex")
 	public void errorOcurance(JoinPoint joinPoint, Exception ex){
 		System.out.println("An Error Has Occured");
