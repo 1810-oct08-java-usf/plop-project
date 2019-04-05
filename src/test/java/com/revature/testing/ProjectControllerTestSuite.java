f367cf398ede5ddd1c7500a0034feade5cf7bce9
AlonzoMuncy <james@alonzomuncy.com>
2019-04-05 09:57:34 -0400

    added project controller test suite

---
+++ src/test/java/com/revature/testing/ProjectControllerTestSuite.java
@@ -0,0 +1,13 @@
+package com.revature.testing;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.revature.exceptions.ProjectNotFoundException;
+import com.revature.models.Project;
+
+public class ProjectControllerTestSuite {//For testing the ProjectController class --Alonzo Muncy 2019/04/05
+
+}
+
+
