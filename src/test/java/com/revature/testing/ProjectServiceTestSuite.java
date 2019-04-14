05464d06a8b4dd109e0d34adb753c4d50bb0d168
AlonzoMuncy <james@alonzomuncy.com>
2019-04-14 14:43:01 -0400

    updated project service test suite

--- src/test/java/com/revature/testing/ProjectServiceTestSuite.java
+++ src/test/java/com/revature/testing/ProjectServiceTestSuite.java
@@ -1,7 +1,12 @@
 package com.revature.testing;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -11,10 +16,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.revature.models.Project;
+import com.revature.models.ProjectDTO;
 import com.revature.repositories.ProjectRepository;
 import com.revature.services.FileService;
 import com.revature.services.ProjectService;
@@ -52,6 +61,12 @@ public class ProjectServiceTestSuite {
 	private FileService testFileService;
 	
 	/**
+	 * A simulated ProjectDTO
+	 */
+	@Mock
+	private ProjectDTO mockProjectDTO;
+	
+	/**
 	 * A simulated List<Project>; this can also be accomplished using a spy.
 	 */
 	@Mock
@@ -61,7 +76,32 @@ public class ProjectServiceTestSuite {
 	 * A simulated Project; holds data for the test methods to access during assertion.
 	 */
 	@Mock
-	private Project dummyProject = new Project();
+	private Project dummyProject;
+	
+	/**
+	 * A simulated Project; holds data for the test methods to access during assertion.
+	 */
+	@Mock
+	private Project dummySavedProject;
+	
+	/**
+	 * A mock file
+	 */
+	@Mock
+	private File mockFile;
+	/**
+	 * Can't spy or mock final classes
+	 */
+	Optional<Project> optionalProject;
+	
+	String dummyString = "Something";
+	
+	/**
+	 * A mock list of strings
+	 */
+	@Mock
+	ArrayList<String> mockListString;
+	
 	
 	@Before
 	public void preTestInit() {			
@@ -140,4 +180,64 @@ public class ProjectServiceTestSuite {
 	public void shouldReturnLinkedList() {
 		assertThat(classUnderTest.findAllProjects()).isInstanceOf(LinkedList.class);
 	}
+	/**
+	 * Check if we can update all parts of a project
+	 */
+	@Test
+	public void testUpdateProject() {
+		optionalProject = Optional.of(dummySavedProject);
+		when(testRepo.findById("97")).thenReturn(optionalProject);
+		when(dummyProject.getName()).thenReturn(dummyString);
+		when(dummyProject.getBatch()).thenReturn(dummyString);
+		when(dummyProject.getTrainer()).thenReturn(dummyString);
+		when(dummyProject.getGroupMembers()).thenReturn(mockListString);
+		when(dummyProject.getScreenShots()).thenReturn(mockListString);
+		when(dummyProject.getZipLinks()).thenReturn(mockListString);
+		when(dummyProject.getDescription()).thenReturn(dummyString);
+		when(dummyProject.getTechStack()).thenReturn(dummyString);
+		when(dummyProject.getStatus()).thenReturn(dummyString);
+		when(dummyProject.getOldProject()).thenReturn(dummySavedProject);
+		assertTrue(classUnderTest.updateProject(dummyProject, "97"));
+	}
+	
+	/**
+	 * Test if we can create a project from a DTO. We need the lists and such to properly mock the implementation. 
+	 * TODO: Find a better way to test this thing. Good Luck.
+	 */
+	
+	@Test
+	public void testCreateProjectFromDTO(){
+		
+		ArrayList<MultipartFile> listMultipartFile = new ArrayList<MultipartFile>();
+		
+		MultipartFile mockMultipartFile = new MockMultipartFile("data", "filename.txt", "text/plain", "some xml".getBytes());
+		
+		ArrayList<String> listZipLink = new ArrayList<String>();
+		listZipLink.add("link");
+		listMultipartFile.add(mockMultipartFile);
+		
+		when(mockProjectDTO.getName()).thenReturn(dummyString);
+		when(mockProjectDTO.getBatch()).thenReturn(dummyString);
+		when(mockProjectDTO.getTrainer()).thenReturn(dummyString);
+		when(mockProjectDTO.getGroupMembers()).thenReturn(mockListString);
+		when(mockProjectDTO.getDescription()).thenReturn(dummyString);
+		when(mockProjectDTO.getTechStack()).thenReturn(dummyString);
+		when(mockProjectDTO.getStatus()).thenReturn(dummyString);
+		when(mockProjectDTO.getScreenShots()).thenReturn(listMultipartFile);
+		when(mockProjectDTO.getZipLinks()).thenReturn(listZipLink);
+		when(testStorage.store(mockMultipartFile)).thenReturn(dummyString);
+		
+		
+		try {
+			when(testFileService.download("link/archive/master.zip")).thenReturn(mockFile);
+			
+			when(testStorage.store(mockFile)).thenReturn(dummyString);
+		assertTrue(classUnderTest.createProjectFromDTO(mockProjectDTO) instanceof Project); 
+	
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
 }
