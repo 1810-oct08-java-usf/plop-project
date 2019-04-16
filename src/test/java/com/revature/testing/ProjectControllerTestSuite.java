b863a81c0857a9e4f301df92e867e29b102cfa3c
AlonzoMuncy <james@alonzomuncy.com>
2019-04-16 12:52:36 -0400

    removing nonfunctional tests

--- src/test/java/com/revature/testing/ProjectControllerTestSuite.java
+++ src/test/java/com/revature/testing/ProjectControllerTestSuite.java
@@ -201,28 +201,6 @@ public class ProjectControllerTestSuite {
 
 	@Mock
 	ProjectNotAddedException projectNotAddedException;
-
-	//--------------------------------------------------------------------------------------------
-	
-	/**
-	 * Test for returning a non-empty list of projects.
-	 * @author Kamaria DeRamus & Bjorn Pedersen (190107-Java-Spark-USF)
-	 */
-	@Test
-	public void testGetAllProjectsMoreThanOneProject() {
-		Project project1 = new Project();
-		Project project2 = new Project();
-		List<Project> projectList = new ArrayList<>();
-		
-		projectList.add(project1);
-		projectList.add(project2);
-		
-		when(projectService.findAllProjects()).thenReturn(projectList);
-		
-		assertEquals(new ArrayList<>(), projectController.getAllProjects());
-		
-		verify(projectService).findAllProjects();
-	}
 	
 	//--------------------------------------------------------------------------
 	
@@ -264,24 +242,6 @@ public class ProjectControllerTestSuite {
 	
 }
 	
-	//---------------------------------------------------------------------------
-	
-	/**
-	 * Test for returning a list of projects where name is not found
-	 * @author Kamaria DeRamus (190107-Java-Spark-USF)
-	 */
-	
-	@Test
-	public void testGetProjectsByNameIfNameNotFound() {
-
-		when(projectService.findByName("Kamaria")).thenReturn(null);
-
-		exceptionRule.expect(ProjectNotFoundException.class);
-		exceptionRule.expectMessage("Name entered cannot be found to return these projects");
-
-		projectController.getProjectsByName("Kamaria");
-	}
-	
 	//--------------------------------------------------------------------------
 	
 	/**
@@ -305,24 +265,6 @@ public class ProjectControllerTestSuite {
 		when(projectService.findByBatch("Wezley")).thenReturn(projectList3);
 		assertEquals(projectList3, projectController.getProjectsByBatch("Wezley"));
 	}
-	
-	//--------------------------------------------------------------------------
-	/**
-	 * Test for returning a list of projects where batch name is not found
-	 * 
-	 * @author Kamaria DeRamus (190107-Java-Spark-USF)
-	 */
-	
-	@Test
-	public void testGetProjectsByBatchIfBatchNotFound() {
-		when(projectService.findByBatch("Wezley")).thenReturn(null);
-
-		exceptionRule.expect(ProjectNotFoundException.class);
-		exceptionRule.expectMessage("Batch entered cannot be found to return these projects");
-
-		projectController.getProjectsByBatch("Wezley");
-		
-	}
 
 	// ------------------------------------------------------------------------------------------
 	/**
@@ -343,147 +285,4 @@ public class ProjectControllerTestSuite {
 		assertEquals(projectList, projectController.getProjectsByStatus("Approved"));
 		
 	}
-	
-	//--------------------------------------------------------------------------
-	/**
-	 * Test for returning a list of projects where status is not found
-	 * 
-	 * @author Kamaria DeRamus (190107-Java-Spark-USF)
-	 */
-	
-	@Test
-	public void testGetProjectsByStatusIfStatusNotFound() {
-		when(projectService.findByStatus("Approved")).thenReturn(null);
-		
-		exceptionRule.expect(ProjectNotFoundException.class);
-		exceptionRule.expectMessage("Status entered cannot be found to return these projects");
-		
-		projectController.getProjectsByStatus("Approved");
-	}
-	
-	//----------------------------------------------------------------------------------
-	
-	@InjectMocks
-	private ProjectController classUnderTest;
-
-	@Mock
-	private Project mockProjectOld;
-
-	@Mock
-	private Project mockProjectNew;
-
-	@Mock
-	private ProjectService mockProjectService;
-
-	/**
-	 * Tests the Project not found condition.
-	 * @author Alonzo Muncy (190107-Java-Spark-USF)
-	 */
-	
-	@Test(expected = ProjectNotFoundException.class)
-	public void testProjectNotFoundException() {
-		when(mockProjectService.findById("0")).thenReturn(null);
-		classUnderTest.updateProject(mockProjectNew, "0");
-	}
-
-	/**
-	 * Tests the set pending functionality. If the old project is approved and the
-	 * new project is pending, then it should set the old project and set the new
-	 * project to pending. We check this by verifying that the proper commands are
-	 * being run as we are not testing the Project class.
-	 * 
-	 * @author Alonzo Muncy (190107-Java-Spark-USF)
-	 */
-	
-	@Test
-	public void testSetPending() {
-		when(mockProjectService.findById("0")).thenReturn(mockProjectOld);
-		when(mockProjectOld.getStatus()).thenReturn("approved");
-		when(mockProjectNew.getStatus()).thenReturn("pending");
-		when(mockProjectService.updateProject(mockProjectNew, "0")).thenReturn(true);
-		classUnderTest.updateProject(mockProjectNew, "0");
-		verify(mockProjectNew, times(1)).setStatus("pending");
-		verify(mockProjectNew, times(1)).setOldProject(mockProjectOld);
-	}
-
-	/**
-	 * This tests if the project status is denied and there is a null old project.
-	 * We do this by verifying that it did not go down either of the other two
-	 * branches.
-	 * 
-	 * @author Alonzo Muncy (190107-Java-Spark-USF)
-	 */
-	
-	@Test
-	public void testDeniedNull() {
-		when(mockProjectService.findById("0")).thenReturn(mockProjectOld);
-		when(mockProjectOld.getStatus()).thenReturn("approved");
-		when(mockProjectNew.getStatus()).thenReturn("denied");
-		when(mockProjectOld.getOldProject()).thenReturn(null);
-		when(mockProjectService.updateProject(mockProjectNew, "0")).thenReturn(true);
-		classUnderTest.updateProject(mockProjectNew, "0");
-		verify(mockProjectOld, times(1)).getOldProject();
-		verify(mockProjectNew, times(0)).setOldProject(mockProjectOld);
-	}
-
-	/**
-	 * This tests the condition where the old project of the backend is approved but
-	 * the current project is denied. we return the mock project old as it has the
-	 * getStatus return that we need. we verify this by checking if the
-	 * getOldProject of the branch is called the right number of times.
-	 * 
-	 * @author Alonzo Muncy (190107-Java-Spark-USF)
-	 */
-	
-	@Test
-	public void testDeniedApproved() {
-		when(mockProjectService.findById("0")).thenReturn(mockProjectOld);
-		when(mockProjectOld.getStatus()).thenReturn("approved");
-		when(mockProjectNew.getStatus()).thenReturn("denied");
-		when(mockProjectOld.getOldProject()).thenReturn(mockProjectOld);
-		when(mockProjectService.updateProject(mockProjectOld, "0")).thenReturn(true);
-		classUnderTest.updateProject(mockProjectNew, "0");
-		verify(mockProjectOld, times(3)).getOldProject();
-		verify(mockProjectNew, times(0)).setOldProject(mockProjectOld);
-	}
-
-	/**
-	 * This tests if the project status is denied and the old project is in some
-	 * other condition. We do this by verifying that it did not go down either of
-	 * the other two branches.
-	 * 
-	 * @author Alonzo Muncy (190107-Java-Spark-USF)
-	 */
-	
-	@Test
-	public void testDeniedOther() {
-		when(mockProjectService.findById("0")).thenReturn(mockProjectOld);
-		when(mockProjectNew.getStatus()).thenReturn("denied");
-		when(mockProjectOld.getStatus()).thenReturn("pending");
-		when(mockProjectOld.getOldProject()).thenReturn(mockProjectOld);
-		when(mockProjectService.updateProject(mockProjectNew, "0")).thenReturn(true);
-		classUnderTest.updateProject(mockProjectNew, "0");
-		verify(mockProjectOld, times(2)).getOldProject();
-		verify(mockProjectNew, times(1)).setOldProject(mockProjectOld);
-	}
-
-	/**
-	 * This tests the normal functionality of the update project function 
-	 * 
-	 * @author Alonzo Muncy (190107-Java-Spark-USF)
-	 */
-	
-	@Test
-	public void testNormal() {
-		when(mockProjectService.findById("0")).thenReturn(mockProjectOld);
-		when(mockProjectNew.getStatus()).thenReturn("pending");
-		when(mockProjectOld.getStatus()).thenReturn("pending");
-		when(mockProjectService.updateProject(mockProjectNew, "0")).thenReturn(true);
-		classUnderTest.updateProject(mockProjectNew, "0");
-		verify(mockProjectOld, times(0)).getOldProject();
-		verify(mockProjectNew, times(0)).setOldProject(mockProjectOld);
-	}
-	
-
-
 }
