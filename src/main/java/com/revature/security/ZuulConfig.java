0a3e03985eee64148e8b55bc3ab08016643f0aac
Starkiller1010 <gevega@fullsail.edu>
2019-10-22 14:32:07 -0400

    Refactored/Cleaned ZuulConfig.java
    - Added To Dos for line 45.
    - Removed Author from Javadocs for class
    - Changed all comments into javadoc style comments

--- src/main/java/com/revature/security/ZuulConfig.java
+++ src/main/java/com/revature/security/ZuulConfig.java
@@ -5,27 +5,25 @@ import org.springframework.beans.factory.annotation.Value;
 /**
  * Provides configuration which will be used during Zuul Header Authentication.
  * 
- * @author Sean Doyle (1810-Oct22-Java-USF)
  */
 public class ZuulConfig {
 
-	/*
+	/**
 	 * Indicates the name of the HTTP header that will contain the prefix and Zuul
 	 * header as a value
 	 */
 	@Value("${security.zsign.header:RPM_ZUUL_ACCESS_HEADER}")
 	private String header;
 
-	/*
+	/**
 	 * Indicates the salt value that will be combined with the secret to form the
 	 * hash that will be transferred with the request
 	 */
 	@Value("${security.zsign.salt}")
 	private String salt;
-	/*
-	 * TODO This should be refactored to use an environment variable
+	/**
+	 * Used as a key with the encryption algorithm SHA-512 to generate hash
 	 */
-	// Used as a key with the encryption algorithm SHA-512 to generate hash
 	@Value("${security.zsign.secret}")
 	private String secret;
 
