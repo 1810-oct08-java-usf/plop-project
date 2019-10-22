4a68f805499c77d284f253bb0cb54b1566aeaa6a
Starkiller1010 <gevega@fullsail.edu>
2019-10-22 16:17:06 -0400

    Refactored/Cleaned SecurityCredentialsConfig.java
    - Removed authors from convert method
    - Updated TODO for a multi-lines

--- src/main/java/com/revature/security/SecurityCredentialsConfig.java
+++ src/main/java/com/revature/security/SecurityCredentialsConfig.java
@@ -12,67 +12,66 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
- * This currently checks if the current User is allowed to access a certain end point
- * using their role.
- *
- * @author Sean Doyle (1810-Oct22-Java-USF)
- * @author Josh Jibilian (1810-Oct22-Java-USF)
- * @author Colt Ossoff (1810-Oct22-Java-USF)
- * @author Bronwen Hughes (1810-Oct22-Java-USF)
- * @author John Savath (1810-Oct22-Java-USF)
+ * This currently checks if the current User is allowed to access a certain end
+ * point using their role.
  */
 
 @EnableWebSecurity
-public class SecurityCredentialsConfig extends WebSecurityConfigurerAdapter{
-	
+public class SecurityCredentialsConfig extends WebSecurityConfigurerAdapter {
+
 	private ZuulConfig zuulConfig;
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			// Disables protection against Cross-Site Request Forgery, else requests can't be made from zuul-service
-			.csrf().disable()
-	
-			// Ensures a stateless session; session will not store user information
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-	
-			// Handle any authentication exceptions by sending an Unauthorized 401 response
-			.exceptionHandling()
-			.authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
-			
-			//Add customized filter to check for Zuul header, or if request is to get information from the Actuator
-            .addFilterBefore(new CustomAuthenticationFilter(zuulConfig), UsernamePasswordAuthenticationFilter.class)
-            .authorizeRequests()
-            
-            // Allow GET requests to the "/project" endpoint
-            .mvcMatchers(HttpMethod.GET, "/project").permitAll()
-			
-			// Allow POST requests to the "/auth" and "/auth/users" endpoints
-			.mvcMatchers(HttpMethod.POST, "/auth").permitAll()
-			.mvcMatchers(HttpMethod.POST, "/project").hasAnyRole("ADMIN","USER")
+				// Disables protection against Cross-Site Request Forgery, else requests can't
+				// be made from zuul-service
+				.csrf().disable()
 
-			// Allow only admins to access the h2-console
-			.mvcMatchers("/h2-console/**").hasRole("ADMIN")
-			
-			// Allow unrestricted access to the actuator/info endpoint. 
-			// Otherwise, AWS ELB cannot perform a health check on the instance and it drains the instances.
-			.antMatchers(HttpMethod.GET, "/actuator/info").permitAll()
-			.antMatchers(HttpMethod.GET, "/actuator/health").permitAll()
-			.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-			.antMatchers(HttpMethod.GET, "/actuator/routes").permitAll()
-			
-			// Allow unrestricted access to swagger's documentation
-			.antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**").permitAll()
-			
-			// All other requests must be authenticated
-			.anyRequest().authenticated();
+				// Ensures a stateless session; session will not store user information
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+
+				// Handle any authentication exceptions by sending an Unauthorized 401 response
+				.exceptionHandling()
+				.authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
+
+				// Add customized filter to check for Zuul header, or if request is to get
+				// information from the Actuator
+				.addFilterBefore(new CustomAuthenticationFilter(zuulConfig), UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+
+				// Allow GET requests to the "/project" endpoint
+				.mvcMatchers(HttpMethod.GET, "/project").permitAll()
+
+				// Allow POST requests to the "/auth" and "/auth/users" endpoints
+				.mvcMatchers(HttpMethod.POST, "/auth").permitAll().mvcMatchers(HttpMethod.POST, "/project")
+				.hasAnyRole("ADMIN", "USER")
+
+				// Allow only admins to access the h2-console
+				.mvcMatchers("/h2-console/**").hasRole("ADMIN")
+
+				// Allow unrestricted access to the actuator/info endpoint.
+				// Otherwise, AWS ELB cannot perform a health check on the instance and it
+				// drains the instances.
+				.antMatchers(HttpMethod.GET, "/actuator/info").permitAll()
+				.antMatchers(HttpMethod.GET, "/actuator/health").permitAll().antMatchers(HttpMethod.GET, "/actuator/**")
+				.permitAll().antMatchers(HttpMethod.GET, "/actuator/routes").permitAll()
+
+				// Allow unrestricted access to swagger's documentation
+				.antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security",
+						"/swagger-ui.html", "/webjars/**")
+				.permitAll()
+
+				// All other requests must be authenticated
+				.anyRequest().authenticated();
 
 	}
-	
+
 	@Bean
 	public ZuulConfig zuulConfig() {
 		return new ZuulConfig();
 	}
+
 	@Autowired
 	public void setZuulConfig(ZuulConfig zuulConfig) {
 		this.zuulConfig = zuulConfig;
