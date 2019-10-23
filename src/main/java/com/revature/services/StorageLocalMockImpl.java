0e7164bd36a70078b1e58738826163d777a1a708
Starkiller1010 <gevega@fullsail.edu>
2019-10-23 16:05:54 -0400

    Refactored/cleaned/formatted StorageLocalMockImpl.java
    - Removed author tag
    - Formatted to be in google style
    - Removed TODO comments

--- src/main/java/com/revature/services/StorageLocalMockImpl.java
+++ src/main/java/com/revature/services/StorageLocalMockImpl.java
@@ -24,85 +24,76 @@ import com.revature.util.FileHelper;
 @Profile("local")
 @PropertySource("classpath:credentials.properties")
 public class StorageLocalMockImpl implements StorageService {
-	
-	@Value("${ACCESS_KEY_ID}")
-	private String awsAccessKeyId;
 
-	@Value("${SECRET_ACCESS_KEY}")
-	private String awsSecretAccessKey;
+  @Value("${ACCESS_KEY_ID}")
+  private String awsAccessKeyId;
 
-	@Value("${BUCKET_NAME}")
-	private String bucketName;
+  @Value("${SECRET_ACCESS_KEY}")
+  private String awsSecretAccessKey;
 
-	@Value("${BUCKET_REGION}")
-	private String bucketRegion;
-	
-	@Value("${S3_ENDPOINT}")
-	private String s3EndPoint;
+  @Value("${BUCKET_NAME}")
+  private String bucketName;
 
-	private AWSCredentials credentials;
-	private AmazonS3 s3Client;
+  @Value("${BUCKET_REGION}")
+  private String bucketRegion;
 
-	Logger logger = Logger.getLogger(StorageLocalMockImpl.class);
-	
-	/**
-	 * init draws on environment variables setting up an s3Client used to store objects
-	 * Added @Transactional
-	 * 
-	 * @author Stuart Pratuch (190422-JAVA-SPARK-USF)
-	 */
-	@Override
-	@Transactional
-	@PostConstruct
-	public void init() {
-		credentials = new BasicAWSCredentials(awsAccessKeyId, awsSecretAccessKey);
-		System.out.println("awsAccessKeyId: " + awsAccessKeyId);
-		System.out.println("awsSecretAccessKey: " + awsSecretAccessKey);
-		s3Client = AmazonS3ClientBuilder
-			.standard()
-			.withCredentials(new AWSStaticCredentialsProvider(credentials))
-			.withRegion(bucketRegion)
-			.build();
-	}
-	
-	/**
-	 * store puts an object in the configured s3 bucket
-	 * Added @Transactional
-	 * 
-	 * @param multipartFile the file representation of the object desired to store
-	 * @return the link to the new object
-	 * @author Stuart Pratuch (190422-JAVA-SPARK-USF)
-	 */
-	@Override
-	@Transactional
-	public String store(MultipartFile multipartFile) {
-		System.out.println("inside store(multipartFile) method");
-		try {
-			s3Client.putObject(bucketName, multipartFile.getOriginalFilename(), FileHelper.convert(multipartFile));
-			return  s3EndPoint + '/' + bucketName + '/' + multipartFile.getOriginalFilename();
-		} catch (IOException e) {
-			e.printStackTrace();
-			 return "";
-			
-		}
-	}
+  @Value("${S3_ENDPOINT}")
+  private String s3EndPoint;
 
-	/**
-	 * store puts an object in the configured s3 bucket
-	 * 
-	 * @transactional added
-	 * 
-	 * TODO: Test this once implemented with AWS S3
-	 * 
-	 * @param file the file representation fo the object desired to store
-	 * @return the link to the new object
-	 *@author Stuart Pratuch (190422-JAVA-SPARK-USF)
-	 */
-	@Override
-	@Transactional
-	public String store(File file) {
-		s3Client.putObject(bucketName, file.getName(), file);
-		return s3EndPoint + '/' + bucketName + '/' + file.getName();
-	}
+  private AWSCredentials credentials;
+  private AmazonS3 s3Client;
 
+  Logger logger = Logger.getLogger(StorageLocalMockImpl.class);
+
+  /**
+   * init draws on environment variables setting up an s3Client used to store objects
+   * Added @Transactional
+   */
+  @Override
+  @Transactional
+  @PostConstruct
+  public void init() {
+    credentials = new BasicAWSCredentials(awsAccessKeyId, awsSecretAccessKey);
+    System.out.println("awsAccessKeyId: " + awsAccessKeyId);
+    System.out.println("awsSecretAccessKey: " + awsSecretAccessKey);
+    s3Client =
+        AmazonS3ClientBuilder.standard()
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withRegion(bucketRegion)
+            .build();
+  }
+
+  /**
+   * store puts an object in the configured s3 bucket Added @Transactional
+   *
+   * @param multipartFile the file representation of the object desired to store
+   * @return the link to the new object
+   */
+  @Override
+  @Transactional
+  public String store(MultipartFile multipartFile) {
+    System.out.println("inside store(multipartFile) method");
+    try {
+      s3Client.putObject(
+          bucketName, multipartFile.getOriginalFilename(), FileHelper.convert(multipartFile));
+      return s3EndPoint + '/' + bucketName + '/' + multipartFile.getOriginalFilename();
+    } catch (IOException e) {
+      e.printStackTrace();
+      return "";
+    }
+  }
+
+  /**
+   * store puts an object in the configured s3 bucket
+   *
+   * @transactional added
+   * @param file the file representation fo the object desired to store
+   * @return the link to the new object
+   */
+  @Override
+  @Transactional
+  public String store(File file) {
+    s3Client.putObject(bucketName, file.getName(), file);
+    return s3EndPoint + '/' + bucketName + '/' + file.getName();
+  }
 }
