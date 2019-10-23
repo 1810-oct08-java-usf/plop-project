4d337474af6d58d10128f5b9f29767f7ec0c0caf
Starkiller1010 <gevega@fullsail.edu>
2019-10-23 16:07:34 -0400

    Refactored/Cleaned/Formatted StorageService.java
    - Formatted to Google Style

--- src/main/java/com/revature/services/StorageService.java
+++ src/main/java/com/revature/services/StorageService.java
@@ -4,30 +4,25 @@ import java.io.File;
 
 import org.springframework.web.multipart.MultipartFile;
 
-/**
- * init draws on environment variables setting up an s3Client used to store objects
- */
+/** init draws on environment variables setting up an s3Client used to store objects */
 public interface StorageService {
 
-	/**
-	 * init draws on environment variables setting up an s3Client used to store objects
-	 */
-	void init();
+  /** init draws on environment variables setting up an s3Client used to store objects */
+  void init();
 
-	/**
-	 * store puts an object in the configured s3 bucket
-	 * 
-	 * @param multipartFile the file representation of the object desired to store
-	 * @return the link to the new object
-	 */
-	String store(MultipartFile multipartFile);
+  /**
+   * store puts an object in the configured s3 bucket
+   *
+   * @param multipartFile the file representation of the object desired to store
+   * @return the link to the new object
+   */
+  String store(MultipartFile multipartFile);
 
-	/**
-	 * store puts an object in the configured s3 bucket
-	 * 
-	 * @param file the file representation fo the object desired to store
-	 * @return the link to the new object
-	 */
-	String store(File file);
-
+  /**
+   * store puts an object in the configured s3 bucket
+   *
+   * @param file the file representation fo the object desired to store
+   * @return the link to the new object
+   */
+  String store(File file);
 }
