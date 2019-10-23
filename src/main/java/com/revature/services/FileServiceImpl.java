6705e9159081f4d58cec03fe957f5a14fc59d077
Starkiller1010 <gevega@fullsail.edu>
2019-10-23 14:57:43 -0400

    Formatted FileServiceImpl.java to Google Style

--- src/main/java/com/revature/services/FileServiceImpl.java
+++ src/main/java/com/revature/services/FileServiceImpl.java
@@ -16,35 +16,31 @@ import org.springframework.web.client.RestTemplate;
 
 import com.revature.util.FileHelper;
 
-/**
- * FileServiceImpl implements FileService
- */
+/** FileServiceImpl implements FileService */
 @Service
 public class FileServiceImpl implements FileService {
-	
-	/**
-	 * FileServiceImpl.download simply implements FileService.download
-	 * Added @Transactional
-	 * 
-	 * @param fileURI URI to the file to be downloaded
-	 * @return the downloaded File
-	 * @throws IOException if there is an issue downloading the specified file
-	 * @author Stuart Pratuch (190422-JAVA-SPARK-USF)
-	 */ 
-	@Transactional
-	public File download(String fileURI) throws IOException {
-		
-		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.getMessageConverters().add(new ByteArrayHttpMessageConverter());    
-		HttpHeaders headers = new HttpHeaders();
-		headers.setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM));
-		HttpEntity<String> entity = new HttpEntity<String>(headers);
-		
-		ResponseEntity<byte[]> response = restTemplate.exchange(fileURI, HttpMethod.GET, entity, byte[].class, "1");
-		
-		String fileName = response.getHeaders().getContentDisposition().getFilename();
 
-		return FileHelper.convert(response.getBody(), fileName);
-	}
+  /**
+   * FileServiceImpl.download simply implements FileService.download Added @Transactional
+   *
+   * @param fileURI URI to the file to be downloaded
+   * @return the downloaded File
+   * @throws IOException if there is an issue downloading the specified file
+   */
+  @Transactional
+  public File download(String fileURI) throws IOException {
 
+    RestTemplate restTemplate = new RestTemplate();
+    restTemplate.getMessageConverters().add(new ByteArrayHttpMessageConverter());
+    HttpHeaders headers = new HttpHeaders();
+    headers.setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM));
+    HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+    ResponseEntity<byte[]> response =
+        restTemplate.exchange(fileURI, HttpMethod.GET, entity, byte[].class, "1");
+
+    String fileName = response.getHeaders().getContentDisposition().getFilename();
+
+    return FileHelper.convert(response.getBody(), fileName);
+  }
 }
