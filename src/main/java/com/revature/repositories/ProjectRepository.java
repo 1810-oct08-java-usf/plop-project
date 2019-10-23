859771d69b9da4761cebdb44aaef9d1d5e5057d3
Starkiller1010 <gevega@fullsail.edu>
2019-10-23 16:16:09 -0400

    Formatting ProjectRespository.java

--- src/main/java/com/revature/repositories/ProjectRepository.java
+++ src/main/java/com/revature/repositories/ProjectRepository.java
@@ -13,46 +13,50 @@ import com.revature.models.Project;
 @Repository
 public interface ProjectRepository extends MongoRepository<Project, String> {
 
-	/**
-	 * ProjectRepository.findByName accepts a project name and returns a list of projects with that name
-	 * 
-	 * @param name the name of the project(s) you want to retrieve
-	 * @return a list of projects matching the given name
-	 */
-	public List<Project> findByName(String name);
+  /**
+   * ProjectRepository.findByName accepts a project name and returns a list of projects with that
+   * name
+   *
+   * @param name the name of the project(s) you want to retrieve
+   * @return a list of projects matching the given name
+   */
+  public List<Project> findByName(String name);
 
-	/**
-	 * ProjectRepository.findByBatch accepts a project batch and returns a list of projects with that batch
-	 * 
-	 * @param batch the batch of the project(s) you want to retrieve
-	 * @return a list of projects matching the given batch
-	 */
-	public List<Project> findByBatch(String batch);
+  /**
+   * ProjectRepository.findByBatch accepts a project batch and returns a list of projects with that
+   * batch
+   *
+   * @param batch the batch of the project(s) you want to retrieve
+   * @return a list of projects matching the given batch
+   */
+  public List<Project> findByBatch(String batch);
 
-	/**
-	 * ProjectRepository.findByTrainer accepts a project trainer and returns a list of projects with that trainer
-	 * 
-	 * @param trainer the trainer of the project(s) you want to retrieve
-	 * @return a list of projects matching the given trainer
-	 */
-	public List<Project> findByTrainer(String trainer);
-	
-	public List<Project> findByUserId(Integer userId);
+  /**
+   * ProjectRepository.findByTrainer accepts a project trainer and returns a list of projects with
+   * that trainer
+   *
+   * @param trainer the trainer of the project(s) you want to retrieve
+   * @return a list of projects matching the given trainer
+   */
+  public List<Project> findByTrainer(String trainer);
 
-	/**
-	 * ProjectRepository.findByTechStack accepts a project techStack and returns a list of projects with that techStack
-	 * 
-	 * @param techStack the techStack of the project(s) you want to retrieve
-	 * @return a list of projects matching the given techStack
-	 */
-	public List<Project> findByTechStack(String techStack);
+  public List<Project> findByUserId(Integer userId);
 
-	/**
-	 * ProjectRepository.findByStatus accepts a project status and returns a list of projects with that status
-	 * 
-	 * @param status the status of the project(s) you want to retrieve
-	 * @return a list of projects matching the given status
-	 */
-	public List<Project> findByStatus(String status);
+  /**
+   * ProjectRepository.findByTechStack accepts a project techStack and returns a list of projects
+   * with that techStack
+   *
+   * @param techStack the techStack of the project(s) you want to retrieve
+   * @return a list of projects matching the given techStack
+   */
+  public List<Project> findByTechStack(String techStack);
 
+  /**
+   * ProjectRepository.findByStatus accepts a project status and returns a list of projects with
+   * that status
+   *
+   * @param status the status of the project(s) you want to retrieve
+   * @return a list of projects matching the given status
+   */
+  public List<Project> findByStatus(String status);
 }
