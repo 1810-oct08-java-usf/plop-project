aaf264125d265db450e1a92a83b6898da718360d
Starkiller1010 <gevega@fullsail.edu>
2019-10-22 20:46:14 -0400

    Refactored/Cleaned AccessAuthenticationToken.java
    - Removed @Author tags

--- src/main/java/com/revature/security/AccessAuthenticationToken.java
+++ src/main/java/com/revature/security/AccessAuthenticationToken.java
@@ -6,36 +6,37 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 
 /**
- * This class is designed to act as an authentication for a user if they are correctly accessing the
- * service.
- * @author Sean Doyle (1810-Oct22-Java-USF)
+ * This class is designed to act as an authentication for a user if they are
+ * correctly accessing the service.
  */
 
 public class AccessAuthenticationToken extends AbstractAuthenticationToken {
 
 	private static final long serialVersionUID = 8214607188015458452L;
-	
+
 	private final Object principal;
 	private Object credentials;
-	
-	public AccessAuthenticationToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
+
+	public AccessAuthenticationToken(Object principal, Object credentials,
+			Collection<? extends GrantedAuthority> authorities) {
 		super(authorities);
 		this.principal = principal;
 		this.credentials = credentials;
 		super.setAuthenticated(true); // must use super, as we override
 	}
-	
+
 	public AccessAuthenticationToken(Object principal, Object credentials) {
 		super(null);
 		this.principal = principal;
 		this.credentials = credentials;
 		setAuthenticated(false);
 	}
-	
+
 	@Override
 	public Object getCredentials() {
 		return this.credentials;
 	}
+
 	@Override
 	public Object getPrincipal() {
 		return this.principal;
@@ -56,4 +57,3 @@ public class AccessAuthenticationToken extends AbstractAuthenticationToken {
 		credentials = null;
 	}
 }
-
