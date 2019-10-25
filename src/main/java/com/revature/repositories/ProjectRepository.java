8a33ec8d166804767fb26f3358f75142d4182785
Starkiller1010 <gevega@fullsail.edu>
2019-10-25 09:14:02 -0400

    removed white-space ProjectRespository.java

--- src/main/java/com/revature/repositories/ProjectRepository.java
+++ src/main/java/com/revature/repositories/ProjectRepository.java
@@ -1,10 +1,8 @@
 package com.revature.repositories;
 
 import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import com.revature.models.Project;
 
 /**
