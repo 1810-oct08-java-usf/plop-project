8bac4a44fb07e8dd9d3064fc6a15fff2fef5418a
Starkiller1010 <gevega@fullsail.edu>
2019-10-25 09:11:49 -0400

    removed white-space and Todo ProjectService.java

--- src/main/java/com/revature/services/ProjectService.java
+++ src/main/java/com/revature/services/ProjectService.java
@@ -5,21 +5,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.revature.exceptions.FileSizeTooLargeException;
 import com.revature.exceptions.ProjectNotAddedException;
 import com.revature.models.Project;
 import com.revature.models.ProjectDTO;
 import com.revature.repositories.ProjectRepository;
 
-// TODO include transactional annotations to specify propagation and isolation levels
 /** ProjectService provides an interface to interact with a ProjectRepository */
 @Service
 public class ProjectService {
