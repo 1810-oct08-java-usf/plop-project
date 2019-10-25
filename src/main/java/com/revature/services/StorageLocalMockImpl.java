efcc0a6e3e011bb23036ec9842d433cfda5b00d8
Starkiller1010 <gevega@fullsail.edu>
2019-10-25 09:11:18 -0400

    removed white-space StorageLocalMockImpl.java

--- src/main/java/com/revature/services/StorageLocalMockImpl.java
+++ src/main/java/com/revature/services/StorageLocalMockImpl.java
@@ -2,9 +2,7 @@ package com.revature.services;
 
 import java.io.File;
 import java.io.IOException;
-
 import javax.annotation.PostConstruct;
-
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
@@ -12,7 +10,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
