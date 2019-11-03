225bd3635e9a487b2e225f121944044bdd636436
Starkiller1010 <gevega@fullsail.edu>
2019-11-02 21:02:58 -0500

    Removed all invocations of submitEditRequest
    - By editing, also formatted files to google java format

--- src/main/java/com/revature/controllers/ProjectController.java
+++ src/main/java/com/revature/controllers/ProjectController.java
@@ -1,11 +1,17 @@
 package com.revature.controllers;
 
+import com.revature.exceptions.BadRequestException;
+import com.revature.exceptions.ProjectNotAddedException;
+import com.revature.exceptions.ProjectNotFoundException;
+import com.revature.models.Project;
+import com.revature.models.ProjectDTO;
+import com.revature.models.ProjectErrorResponse;
+import com.revature.services.ProjectService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,21 +23,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import com.revature.exceptions.BadRequestException;
-import com.revature.exceptions.InvalidStatusException;
-import com.revature.exceptions.ProjectNotAddedException;
-import com.revature.exceptions.ProjectNotFoundException;
-import com.revature.models.Project;
-import com.revature.models.ProjectDTO;
-import com.revature.models.ProjectErrorResponse;
-import com.revature.services.ProjectService;
 
-/**
- * The ProjectController maps service endpoints for essential CRUD operations on
- * Projects
- */
+/** The ProjectController maps service endpoints for essential CRUD operations on Projects */
 @RestController
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+// @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ProjectController {
 
   private ProjectService projectService;
@@ -41,7 +36,6 @@ public class ProjectController {
     this.projectService = projectService;
   }
 
- 
   @GetMapping(value = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseStatus(HttpStatus.OK)
   public Project getProjectById(@PathVariable String id) {
@@ -50,62 +44,66 @@ public class ProjectController {
   }
 
   /**
-   * This method allows a user to submit an edit request on one of their projects. Uses HTTP method
-   * POST and only consumes JSON data.
-   *
-   * @param project: The new project object for the submitted edit request.
-   */
-  @PutMapping(value = "/edit", consumes = MediaType.APPLICATION_JSON_VALUE)
-  @ResponseStatus(HttpStatus.OK)
-  public boolean submitEditRequest(@RequestBody Project project) {
-    return projectService.submitEditRequest(project);
-  }
-
-  /**
-   * This method accepts each field of a ProjectDTO object in the form of
-   * multipart form data. A ProjectDTO object is created from the fields and sent
-   * to the service layer to be converted to a Project object and saved. <br>
+   * This method accepts each field of a ProjectDTO object in the form of multipart form data. A
+   * ProjectDTO object is created from the fields and sent to the service layer to be converted to a
+   * Project object and saved. <br>
    * <br>
-   * Added Spring Security annotations to prevent unauthorized users from
-   * accessing database
+   * Added Spring Security annotations to prevent unauthorized users from accessing database
    *
-   * @param name         the name field of the form data
-   * @param batch        the batch field of the form data
-   * @param trainer      the trainer field of the form data
+   * @param name the name field of the form data
+   * @param batch the batch field of the form data
+   * @param trainer the trainer field of the form data
    * @param groupMembers the groupMembers field of the form data
-   * @param screenShots  the screenShots field of the form data
-   * @param zipLinks     the zipLinks field of the form data
-   * @param description  the description field of the form data
-   * @param techStack    the techStack field of the form data
-   * @param status       the status field of the form data
-   * @return project: The Project object derived from ProjectDTO in the service
-   *         layer.
+   * @param screenShots the screenShots field of the form data
+   * @param zipLinks the zipLinks field of the form data
+   * @param description the description field of the form data
+   * @param techStack the techStack field of the form data
+   * @param status the status field of the form data
+   * @return project: The Project object derived from ProjectDTO in the service layer.
    */
-  @PostMapping(value = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(
+      value = "/",
+      consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseStatus(HttpStatus.CREATED)
   // @PreAuthorize("hasRole('USER')")
-  public Project addProject(@RequestParam("name") String name, @RequestParam("batch") String batch,
-      @RequestParam("trainer") String trainer, @RequestParam("groupMembers") List<String> groupMembers,
-      @RequestParam("screenShots") List<MultipartFile> screenShots, @RequestParam("zipLinks") List<String> zipLinks,
-      @RequestParam("description") String description, @RequestParam("techStack") String techStack,
-      @RequestParam("status") String status, @RequestParam("dataModel") List<MultipartFile> dataModel,
+  public Project addProject(
+      @RequestParam("name") String name,
+      @RequestParam("batch") String batch,
+      @RequestParam("trainer") String trainer,
+      @RequestParam("groupMembers") List<String> groupMembers,
+      @RequestParam("screenShots") List<MultipartFile> screenShots,
+      @RequestParam("zipLinks") List<String> zipLinks,
+      @RequestParam("description") String description,
+      @RequestParam("techStack") String techStack,
+      @RequestParam("status") String status,
+      @RequestParam("dataModel") List<MultipartFile> dataModel,
       @RequestParam("userId") Integer userId) {
 
-    ProjectDTO projectDTO = new ProjectDTO.ProjectDTOBuilder().setName(name).setBatch(batch).setTrainer(trainer)
-        .setGroupMembers(groupMembers).setScreenShots(screenShots).setZipLinks(zipLinks).setDescription(description)
-        .setTechStack(techStack).setStatus(status).setDataModel(dataModel).setUserId(userId).build();
+    ProjectDTO projectDTO =
+        new ProjectDTO.ProjectDTOBuilder()
+            .setName(name)
+            .setBatch(batch)
+            .setTrainer(trainer)
+            .setGroupMembers(groupMembers)
+            .setScreenShots(screenShots)
+            .setZipLinks(zipLinks)
+            .setDescription(description)
+            .setTechStack(techStack)
+            .setStatus(status)
+            .setDataModel(dataModel)
+            .setUserId(userId)
+            .build();
 
     return projectService.createProjectFromDTO(projectDTO);
   }
 
   /**
-   * This method is used to delete an entry into the embedded MongoDB based on the
-   * ID <br>
+   * This method is used to delete an entry into the embedded MongoDB based on the ID <br>
    * <br>
    * Uses HTTP method DELETE and only retrieves JSON data <br>
    * <br>
-   * Added Spring Security annotations to prevent unauthorized users from
-   * accessing database
+   * Added Spring Security annotations to prevent unauthorized users from accessing database
    *
    * @param id: String that serves as the id for the project
    */
@@ -117,62 +115,59 @@ public class ProjectController {
   }
 
   /**
-   * This method is used to update an entry into the embedded MongoDB based on the
-   * ID <br>
+   * This method is used to update an entry into the embedded MongoDB based on the ID <br>
    * <br>
-   * Added Spring Security annotations to prevent unauthorized users from
-   * accessing database <br>
+   * Added Spring Security annotations to prevent unauthorized users from accessing database <br>
    * <br>
    * Uses HTTP method PUT. Retrieves and produces JSON data
    *
    * @param project: Requests that the user enters a project
-   * @param id:      String that serves as the id for the project
+   * @param id: String that serves as the id for the project
    */
-  @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+  @PutMapping(
+      produces = MediaType.APPLICATION_JSON_VALUE,
+      consumes = MediaType.APPLICATION_JSON_VALUE)
   @ResponseStatus(HttpStatus.OK)
   @PreAuthorize("hasRole('ADMIN')")
   public Boolean updateProject(@RequestBody Project project) {
-   
+
     return projectService.evaluateProject(project);
   }
 
-  @GetMapping(value="/q", produces="application/json")
+  @GetMapping(value = "/q", produces = "application/json")
   @ResponseStatus(HttpStatus.OK)
-	public List<Project> getProjectFieldValue(@RequestParam("field") String field, @RequestParam("value") String value) {
-		
-		switch(field) {
-		case "status":
-			return projectService.findByStatus(value);
-		case "name":
-			return projectService.findByName(value);
-		case "trainer":
-			return projectService.findByTrainer(value);
-		case "techStack":
-			return projectService.findByTechStack(value);
-		case "batch": 
-			return projectService.findByBatch(value);
-		case "userId":
-			return projectService.findByUserId(Integer.valueOf(value));
-		case "all":
-			return projectService.findAllProjects();
-		default:
-			throw new BadRequestException("Invalid field param value specified!");
-				
-		}
-		
-	}
+  public List<Project> getProjectFieldValue(
+      @RequestParam("field") String field, @RequestParam("value") String value) {
+
+    switch (field) {
+      case "status":
+        return projectService.findByStatus(value);
+      case "name":
+        return projectService.findByName(value);
+      case "trainer":
+        return projectService.findByTrainer(value);
+      case "techStack":
+        return projectService.findByTechStack(value);
+      case "batch":
+        return projectService.findByBatch(value);
+      case "userId":
+        return projectService.findByUserId(Integer.valueOf(value));
+      case "all":
+        return projectService.findAllProjects();
+      default:
+        throw new BadRequestException("Invalid field param value specified!");
+    }
+  }
   /**
-   * This method is used to send a status code into the client based on the
-   * validity of the information sent. <br>
+   * This method is used to send a status code into the client based on the validity of the
+   * information sent. <br>
    * <br>
-   * Exception Handler for Response Status Bad Request which is used for
-   * addProject() [/add] <br>
+   * Exception Handler for Response Status Bad Request which is used for addProject() [/add] <br>
    * <br>
-   * Uses @ExceptionHandler annotation. Creates a new error response
-   * error.setStatus: Defines the value of the status code returned if thrown
-   * (BAD_REQUEST) error.setMessage: Defines a custom message sent to the client
-   * if the exception is thrown error.setTimeStamp: Defines the time this error
-   * was thrown
+   * Uses @ExceptionHandler annotation. Creates a new error response error.setStatus: Defines the
+   * value of the status code returned if thrown (BAD_REQUEST) error.setMessage: Defines a custom
+   * message sent to the client if the exception is thrown error.setTimeStamp: Defines the time this
+   * error was thrown
    */
   @ExceptionHandler
   @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -195,16 +190,14 @@ public class ProjectController {
   }
 
   /**
-   * This method is used to send a status code into the client based on the
-   * validity of the information sent. <br>
+   * This method is used to send a status code into the client based on the validity of the
+   * information sent. <br>
    * <br>
-   * Exception Handler for Invalid Status Response which is used for
-   * updateProject() <br>
+   * Exception Handler for Invalid Status Response which is used for updateProject() <br>
    * <br>
-   * Uses @ExceptionHandler annotation. Creates a new error response
-   * error.setStatus: Defines the value of the status code returned if
-   * thrown(BAD_REQUEST) error.setMessage: Defines a custom message sent to the
-   * client if the exception is thrown error.setTimeStamp: Defines the time this
+   * Uses @ExceptionHandler annotation. Creates a new error response error.setStatus: Defines the
+   * value of the status code returned if thrown(BAD_REQUEST) error.setMessage: Defines a custom
+   * message sent to the client if the exception is thrown error.setTimeStamp: Defines the time this
    * error was thrown
    */
   @ExceptionHandler
